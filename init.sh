06b59cf2321775bd9d98deb48629ffa530117c21
ryanemax <ryanemax@hotmail.com>
2016-08-10 07:45:05 +0800

    rebuilding site Wed Aug 10 07:45:05 CST 2016

--- init.sh
+++ init.sh
@@ -21,10 +21,10 @@ git checkout master
 rm -rf public
 
 # Add the gh-pages branch of the repository. It will look like a folder named public
-git subtree add --prefix=public git@github.com:ryanemax/losttemple.git gh-pages --squash
+git subtree add --prefix=public git@github.com:ryanemax/ryanemax.github.io.git gh-pages --squash
 
 # Pull down the file we just committed. This helps avoid merge conflicts
-git subtree pull --prefix=public git@github.com:ryanemax/losttemple.git gh-pages
+git subtree pull --prefix=public git@github.com:ryanemax/ryanemax.github.io.git gh-pages
 
 # Run hugo. Generated site will be placed in public directory (or omit -t ThemeName if you're not using a theme)
 hugo
@@ -37,4 +37,4 @@ git add -A
 git commit -m "Updating site" && git push origin master
 
 # Push the public subtree to the gh-pages branch
-git subtree push --prefix=public git@github.com:ryanemax/losttemple.git gh-pages
+git subtree push --prefix=public git@github.com:ryanemax/ryanemax.github.io.git gh-pages
