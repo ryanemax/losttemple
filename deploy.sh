9bac9d0cdfbe145f2777cb0f30e1b1785c1ce8fd
root <root@debian>
2016-11-24 09:58:35 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-echo -e "\033[0;32mCopying *.md from ~/Note to Blog...\033[0m"
+echo -e "\033[0;32mCopying *.md from ../note to Blog...\033[0m"
 # 如果需要隐藏文章
 # 方式一：最前方加. 例如：
 # .心理学三级学习计划.md
 # .secret.md
 # 方式二：编辑exclude文件./ignore.list
-rsync -aztP --exclude-from "ignore.list" --delete ~/Note/ content
+rsync -aztP --exclude-from "ignore.list" --delete ../note/ content
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
@@ -25,5 +25,5 @@ if [ $# -eq 1 ]
 	  git push origin master
 	  git subtree push --prefix=public https://github.com/ryanemax/ryanemax.github.io.git gh-pages
 	  # Push source and build repos to Coding.net.
-	  git push coding master
-	  git subtree push --prefix=public https://git.coding.net/ryn/coding-pages.git gh-pages
+	  # git push coding master
+	  # git subtree push --prefix=public https://git.coding.net/ryn/coding-pages.git gh-pages
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo -e "\033[0;32mCopying *.md from ~/Note to Blog...\033[0m"
 # .心理学三级学习计划.md
 # .secret.md
 # 方式二：编辑exclude文件./ignore.list
-rsync -aztP --exclude-from "ignore.list" --delete ~/Note/ content
+rsync -aztP --exclude-from "ignore.list" --delete ../note/ content
 
 #hugo server -w -D -t material-design 
-hugo server -w -D -t base16-ryn
+hugo server --bind 0.0.0.0 -w -D -t base16-ryn --baseUrl http://anasit.com:1313/
