3d55163faeabd57cb846e08892a1bf2b6006c549
ryanemax <ryanemax@hotmail.com>
2017-02-15 15:25:42 +0800

    blog

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
-echo -e "\033[0;32mCopying *.md from ../note to Blog...\033[0m"
 # 如果需要隐藏文章
 # 方式一：最前方加. 例如：
 # .心理学三级学习计划.md
 # .secret.md
 # 方式二：编辑exclude文件./ignore.list
 rsync -aztP --exclude-from "ignore.list" --delete ../note/ content
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo -D
@@ -23,7 +20,15 @@ if [ $# -eq 1 ]
 
 	  # Push source and build repos.
 	  git push origin master
-	  git subtree push --prefix=public https://github.com/ryanemax/ryanemax.github.io.git gh-pages
+
+    #### 分支部署参考：http://www.cnblogs.com/ajianbeyourself/p/5415131.html ####
+		# git remote add -f blog https://github.com/ryanemax/ryanemax.github.io.git
+		# git subtree add --prefix=public blog gh-pages --squash
+		# git subtree pull --prefix=public blog gh-pages
+	  git subtree push --prefix=public blog gh-pages
 	  # Push source and build repos to Coding.net.
 	  # git push coding master
-	  git subtree push --prefix=public https://git.coding.net/ryn/coding-pages.git gh-pages
+		# git remote add -f blog-coding https://git.coding.net/ryn/blog.git
+		# git subtree add --prefix=public blog-coding gh-pages --squash
+		# git subtree pull --prefix=public blog-coding gh-pages
+	  git subtree push --prefix=public blog-coding gh-pages
