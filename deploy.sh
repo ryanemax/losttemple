54d3c6a76357b6d17ad71fc949781fba2e0f4a0b
AnasCloud <info@anasit.com>
2017-04-27 00:10:50 +0800

    rebuilding site Thu Apr 27 00:10:50 CST 2017

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ if [ $# -eq 1 ]
 	  git commit -m "$msg"
 
 	  # Push source and build repos.
-	  git push origin master
+	  git push -f origin master
 	  git subtree push --prefix=public https://github.com/ryanemax/ryanemax.github.io.git gh-pages
 	  # Push source and build repos to Coding.net.
 	  # git push coding master
