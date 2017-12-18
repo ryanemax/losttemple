0433f3998178982e4b73e296989bdaa025ddce09
RyaneMax <ryanemax@gmail.com>
2017-12-18 16:48:48 +0800

    update

--- deploy.ps1
+++ deploy.ps1
@@ -5,5 +5,9 @@ git add -A
 git commit -m "update"
 git push origin master
 
-git subtree push --prefix=public blog gh-pages
-git subtree push --prefix=public blog-coding gh-pages
+# publish-ghpages.md
+git checkout master # you can avoid this line if you are in master...
+git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f blog-coding gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
