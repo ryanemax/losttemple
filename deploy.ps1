cd76b1f18a2dd2e755af797ae0f2e05d90a52dc4
RyaneMax <ryanemax@gmail.com>
2017-12-18 16:58:21 +0800

    update

--- deploy.ps1
+++ deploy.ps1
@@ -9,5 +9,5 @@ git push origin master
 git checkout master # you can avoid this line if you are in master...
 git subtree split --prefix public -b gh-pages # create a local gh-pages branch containing the splitted output folder
 git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
-git push -f blog-coding gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f blog-coding gh-pages:master # force the push of the gh-pages branch to the remote gh-pages branch at origin
 git branch -D gh-pages # delete the local gh-pages because you will need it: ref
